851c43e028e51ded73e4cd4806231b94d9665b05
mubeenfatima717 <h72149394@gmail.com>
2025-09-16 19:05:01 +0500

    Initial commit of project files

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -5,12 +5,18 @@ import { getFirestore } from "firebase/firestore";
 
 // web app's Firebase configuration
 const firebaseConfig = {
- apiKey: "AIzaSyDP3hszI2Ki_e6U1NT2XQFSE8q4vPanG1A",
-  authDomain: "speakup-66d6a.firebaseapp.com",
-  projectId: "speakup-66d6a",
-  storageBucket: "speakup-66d6a.firebasestorage.app",
-  messagingSenderId: "865218276961",
-  appId: "1:865218276961:web:f2a362bdad944ad1828420"
+//  apiKey: "AIzaSyDP3hszI2Ki_e6U1NT2XQFSE8q4vPanG1A",
+//   authDomain: "speakup-66d6a.firebaseapp.com",
+//   projectId: "speakup-66d6a",
+//   storageBucket: "speakup-66d6a.firebasestorage.app",
+//   messagingSenderId: "865218276961",
+//   appId: "1:865218276961:web:f2a362bdad944ad1828420",
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
